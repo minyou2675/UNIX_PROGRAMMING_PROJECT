48bb02f7de6eeaa305a05f5de83514f2803892bf
201711008 <kymyy2675@gmail.com>
2022-12-19 15:57:29 +0900

    updt

--- final/1.c
+++ final/1.c
@@ -6,8 +6,8 @@
 int cnt = 0;
 
 void sig_handler(int signo){
-
-    psignal(signo,"CHandler is called  %d time(s)",cnt);
+   cnt ++;
+   fprintf(stderr,"CHanlder is called %d time(s)",cnt);
 
 }
 
@@ -16,7 +16,7 @@ int main(){
     struct sigaction act;
     sigemptyset(&act.sa_mask);
     sigaddset(&act.sa_mask, SIGQUIT);
-    act.sa_handler = sig_hanlder;
+    act.sa_handler = sig_handler;
     if(sigaction(SIGINT, &act, (struct sigaction *)NULL) < 0){
         perror("sigaction");
         exit(1);
@@ -24,6 +24,6 @@ int main(){
 
     while(1){
         printf("I'm in infinite loop");
-        pause()
+        pause();
     }
 }
